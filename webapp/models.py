07aeba98f623a721246acdcb7d265327b51a734f
Korenchuk Rustam <smohghtxx@gmail.com>
2024-06-24 15:57:40 +0600

    сделали модель Таск, сделали миграции и зарегали их в админке

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    status_choices = [
+        ('new', "Новая"),
+        ('in_progress', "В процессе"),
+        ('done', "Сделано"),
+    ]
+
+    status = models.CharField(max_length=30, choices=status_choices, default="New")
+    description = models.CharField(max_length=200)
+    task_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.description
