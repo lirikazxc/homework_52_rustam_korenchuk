db9ebcfd0f95f7ffb315a99b9043560e8b10c281
Korenchuk Rustam <smohghtxx@gmail.com>
2024-06-24 16:09:17 +0600

    написали основные вьюшки

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from webapp.models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, "index.html", {'tasks', tasks})
+
+
+def create_task(request):
+    if request.method == "POST":
+        description = request.POST.get("description")
+        status = request.POST.get("status")
+        task_date = request.POST.get("task_date")
+        Task.objects.create(description=description, status=status, task_date=task_date)
+        return redirect("index")
+    return render(request, "task_create.html")
